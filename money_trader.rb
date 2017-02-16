703ece961dd33f7c9673b40d5d8b83324d1885d3
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-02-16 16:10:49 -0500

    added two functions to confirm that different currencies cannot be added together and they are not equal to each other.

--- money_trader.rb
+++ money_trader.rb
@@ -3,7 +3,7 @@ require_relative "currency_converter"
 
 user_money = CurrencyAmount.new(50.0, "USD")
 more_user_money = CurrencyAmount.new(25.0, "USD")
-jordanian_money = CurrencyAmount.new(30.0, "USD")
+jordanian_money = CurrencyAmount.new(30.0, "JOD")
 money_trader = CurrencyConverter.new #nothing yet
 final_amount = user_money+(more_user_money)
 puts "currency addition= $#{final_amount.amount}"
@@ -12,3 +12,7 @@ puts final_amount.currency_code
 less_money= user_money - more_user_money
 puts "currency subtraction= $#{less_money.amount}"
 puts less_money.currency_code
+
+puts money_error = user_money + jordanian_money
+
+puts user_money == jordanian_money
